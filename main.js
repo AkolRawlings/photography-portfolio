e4dc918c86df9bd80f0ad90d47e041eb0eaf1dd6
AkolRawlings <akolrawlings@gmail.com>
2023-06-27 15:09:15 +0300

    another commit

--- main.js
+++ main.js
@@ -4,3 +4,20 @@ window.addEventListener("scroll", () => {
     .querySelector("nav")
     .classList.toggle("window-scrolled", window.scrollY > 0);
 });
+
+// conatact buttons (circular texts)
+const textButtons = document.querySelectorAll(".contact_btn");
+
+textButtons.forEach((textButton) => {
+  let text = textButton.querySelector("p");
+
+  text.innerHTML
+    .split("")
+    .map(
+      (character, index) =>
+        `<span style="transform: rotate(${
+          index * 12
+        } deg))">${character}</span>`
+    )
+    .join("");
+});
