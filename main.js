8cc27601fad5be80b05d0bc8ac1e1056466e9a25
AkolRawlings <akolrawlings@gmail.com>
2023-06-29 11:35:00 +0300

    gallery section commit one

--- main.js
+++ main.js
@@ -5,19 +5,35 @@ window.addEventListener("scroll", () => {
     .classList.toggle("window-scrolled", window.scrollY > 0);
 });
 
-// conatact buttons (circular texts)
-const textButtons = document.querySelectorAll(".contact_btn");
+// // conatact buttons (circular texts)
+// const textButtons = document.querySelectorAll(".contact_btn");
 
-textButtons.forEach((textButton) => {
-  let text = textButton.querySelector("p");
+// textButtons.forEach((textButton) => {
+//   let text = textButton.querySelector("p");
 
-  text.innerHTML
-    .split("")
-    .map(
-      (character, index) =>
-        `<span style="transform: rotate(${
-          index * 12
-        } deg))">${character}</span>`
-    )
-    .join("");
+//   text.innerHTML
+//     .split("")
+//     .map(
+//       (character, index) =>
+//         `<span style="transform: rotate(${
+//           index * 12
+//         } deg))">${character}</span>`
+//     )
+//     .join("");
+// });
+
+// swiper js
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    599: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+  },
 });
